cc7cd17ae01d805922a88f5b7bbe51a10eae774a
khushi <145266963+khushi519g5@users.noreply.github.com>
2026-06-18 18:59:57 +0530

    package.json

--- backend/routes/activityRoutes.js
+++ backend/routes/activityRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Activity = require("../models/Activity");
+const Activity = require("../models/activity");
 
 // GET ALL ACTIVITY
 router.get("/", async (req, res) => {
