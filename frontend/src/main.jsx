664f6b108f34bf56765b5ec201a8a69849b5e89e
khushi <145266963+khushi519g5@users.noreply.github.com>
2026-06-29 20:10:44 +0530

    hope

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'; // if you have custom CSS
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
